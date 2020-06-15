674541035ed9c096c8f51d47b9a1d5bc7c6ccdca
mmp <mmp@mmp.mmp>
2020-06-15 16:14:04 +0800

    update faviconHash.py

--- ShodanFaviconHash/faviconHash.py
+++ ShodanFaviconHash/faviconHash.py
@@ -1,12 +1,29 @@
+#coding=utf8
+import sys
 import requests
 import mmh3
-import sys
+from bs4 import BeautifulSoup
+
+
+def getURL(html):
+    soup = BeautifulSoup(html, 'lxml')
+    links = soup.find_all('link')
+    try:
+        for l in links:
+            if 'favicon.ico' in l.get('href'):
+                return l.get('href')
+    except Exception:
+        return False
+
 
 if len(sys.argv) < 2:
     print 'Usage:\n\tpython %s url' % sys.argv[0]
     sys.exit(0)
 
-url = sys.argv[1].rstrip('/')
-favicon = requests.get(url + '/favicon.ico').content
+url = sys.argv[1]
+fav_url = getURL(requests.get(url, verify=False).content)
+if not fav_url:
+    fav_url = '/favicon.ico'
+favicon = requests.get(url + fav_url, verify=False).content
 hash = mmh3.hash(favicon.encode('base64'))
 print '[+] %s/favicon.ico ==> %s' % (url, hash)
